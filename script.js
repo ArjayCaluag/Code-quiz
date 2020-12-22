f3c105eb440d0008d26323d0206320f4046cdc79
Ron-Arjay Caluag <arjaycaluag@gmail.com>
2020-12-21 23:23:04 -0800

    Import variables from DOM to js file

---
+++ script.js
@@ -0,0 +1,9 @@
+// Import variables from Dom, our pointers
+let timerEl = document.querySelector("#timer")
+let startScreenEl = document.querySelector("#start-screen")
+let questionZoneEl = document.querySelector("#question-zone")
+let finalZoneEl = document.querySelector("#final-zone")
+let questionTitleEl = document.querySelector("#question-title")
+let questionChoicesEl = document.querySelector("#question-choices")
+let finalScoreEl = document.querySelector("#final-score")
+let initialsEl = document.querySelector("#initials")
